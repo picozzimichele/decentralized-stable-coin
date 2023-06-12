d74e0a83dcff1e789654d7ad7e211f01b8ae2816
Michele Picozzi <michele.picozzi@hotmail.it>
2023-06-12 17:51:13 +0100

    setting up project

---
+++ README.md
@@ -0,0 +1,7 @@
+1. (Relative Stability) Anchored or pegged to USD$ -> 1.00$
+    - powered by chainlink feed
+2. Stability Mechanism is algorithmic, decentralized
+    - people can mint stablecoins only with enough collateral
+3. Collateral: Exogenous (Crypto)
+    - wETH
+    - wBTC
---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/*
+ * @title Decentralized Stable Coin
+ * @author Michele Picozzi
+ * @dev A stable coin that is backed by a basket of crypto assets
+ * @notice This contract is a work in progress and is not ready for production
+ * Collateral: Exogenous BTC & ETH
+ * Minting: Algorithmic
+ * Relative Stability: 1:1 with USD
+ *
+ * This is the contract meant to be governed by DSC Engine. This contract is just the ERC20 implementation of our system.
+ */
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        //this uses the burn function from ERC20Burnable parent class to burn the tokens
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
